21e7ddc1d71ee173ee600b9ea5ee622387059399
koleks92 <koleks92@gmail.com>
2024-07-08 19:12:35 +0200

    feat: get input from index submit

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,14 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+  // Create converHandler
   let convertHandler = new ConvertHandler();
+
+  // Create route for index submit
+  app.route('/api/convert').get((req, res) => {
+    const input = req.query.input;
+
+    console.log(input);
+  })
 
 };
