c4b98b47ef6324a745a8a8d437a8b7f90ce1bb64
koleks92 <koleks92@gmail.com>
2024-07-10 10:12:21 +0200

    feat: slice insed of regex

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,32 +1,59 @@
 function ConvertHandler() {
     this.getNum = function (input) {
-        // Regular expression to match the number part
-        let match = input.match(/^(\d+(\.\d+)?)/);
-
-        if (match) {
-            return match[0];
+        const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let i = 0;
+  
+        for (i; i < input.length; i++) {
+          if (alphabet.includes(input[i])) {
+            break;
+          }
+        };
+  
+        let number = input.slice(0,i);
+  
+        if (!number) {
+          return 1;
         } else {
-            if (input == this.getUnit(input)) {
-                return 1;
-            } else {
-                return false;
-            }
-        }
-    };
-
-    this.getUnit = function (input) {
-        // Regular expression to match the unit part
-        let match = input.match(/(\d+(\.\d+)?)?([a-zA-Z]+)$/);
-
-        const possibleUnits = ["kg", "gal", "L", "mi", "lbs", "km"];
-
-        if (match && possibleUnits.includes(match[3])) {
-            return match[3];
-        } else {
+          try {
+            return parseFloat(number);
+          } catch (err) {
             return false;
+          }
         }
     };
-
+  
+    this.getUnit = function (input) {
+      const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let i = 0;
+  
+      for (i; i < input.length; i++) {
+        if (alphabet.includes(input[i])) {
+          break;
+        }
+      };
+  
+      let unit = input.slice(i);
+  
+      const possibleUnits = ["kg", "gal", "L", "mi", "lbs", "km"];
+  
+      if (!unit) {
+        return false
+      } else {
+        try {
+          if (unit == 'L' || unit == 'l') {
+            return 'L'
+          } else if (possibleUnits.includes(unit.toLowerCase())) {
+            return unit.toLowerCase();
+          } else {
+            return false;
+          }
+        } catch (err) {
+          return false
+        }
+        
+      }
+    };
+  
     this.getReturnUnit = function (initUnit) {
         switch (initUnit) {
           case 'kg': return 'lbs';
@@ -37,7 +64,7 @@ function ConvertHandler() {
           case 'L': return 'gal';
         }
     };
-
+  
     this.spellOutUnit = function (unit) {
         switch (unit) {
             case 'kg': return 'kilograms';
@@ -48,14 +75,14 @@ function ConvertHandler() {
             case 'L': return 'liters';
         };
     };
-
+  
     this.convert = function (initNum, initUnit) {
         const galToL = 3.78541;
         const lbsToKg = 0.453592;
         const miToKm = 1.60934;
-
+  
         let result;
-
+  
         if (initUnit == 'kg') {
             result = (initNum / lbsToKg);
         } else if (initUnit == 'lbs') {
@@ -72,13 +99,14 @@ function ConvertHandler() {
         
         return result.toFixed(5);
     };
-
+  
     this.getString = function (initNum, initUnit, returnNum, returnUnit) {
         initUnitString = this.spellOutUnit(initUnit);
         returnUnitString = this.spellOutUnit(returnUnit);
-
+  
         return `${initNum} ${initUnitString} converts to ${returnNum} ${returnUnitString}`;
     };
-}
-
-module.exports = ConvertHandler;
+  }
+  
+  module.exports = ConvertHandler;
+  
